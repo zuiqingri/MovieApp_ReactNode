66e8933d046b4266fb889ab609e40d3b18804504
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-13 11:05:12 +0800

    Created Boilerplate and made MoveApp landing page(1)

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL='https://api.themoviedb.org/3/';
+export const API_KEY='06480ab36292a9f04f57074b7aa1213f'
+export const IMAGE_BASE_URL='https://image.tmdb.org/t/p/'
