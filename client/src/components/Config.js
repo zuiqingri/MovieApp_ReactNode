0b141818b3e68597c4df5a08680367ca0648ee21
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-13 13:01:15 +0800

    Creating Movie Detail Page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -4,3 +4,11 @@ export const USER_SERVER = '/api/users';
 export const API_URL='https://api.themoviedb.org/3/';
 export const API_KEY='06480ab36292a9f04f57074b7aa1213f'
 export const IMAGE_BASE_URL='https://image.tmdb.org/t/p/'
+
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
